dc09f78007d2568b045763dc7a99b1bb26f4b125
Dima <dima.scherbatyuk@gmail.com>
2022-09-18 16:24:08 +0300

    na: import PGP key

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -3,8 +3,26 @@
 #https://assafmo.github.io/2019/05/02/ppa-repo-hosted-on-github.html
 
 EMAIL=dmytro.shcherbatiuk@netstalker.io
-PPA_GPG_PASSPHRASE=Scherb@tyuk1986
+#GPG_PASSPHRASE=Scherb@tyuk1986
 GITHUB_USERNAME=net-stalker
+
+assert_non_empty() {
+  name=$1
+  value=$2
+  if [[ -z "$value" ]]; then
+    echo "::error::Invalid Value: $name is empty." >&2
+    exit 1
+  fi
+}
+
+assert_non_empty "GPG_PRIVATE_KEY" "$GPG_PRIVATE_KEY"
+assert_non_empty "GPG_PASSPHRASE" "$GPG_PASSPHRASE"
+
+echo "::info::Importing GPG private key"
+GPG_KEY_ID=$(echo "$GPG_PRIVATE_KEY" | gpg --import-options show-only --import | sed -n '2s/^\s*//p')
+echo $GPG_KEY_ID
+echo "$GPG_PRIVATE_KEY" | gpg --batch --passphrase "$GPG_PASSPHRASE" --import
+echo "::info::End Importing"
 
 echo "::info::Creating the KEY.gpg file"
 gpg --armor --export "$EMAIL" >./KEY.gpg
@@ -15,8 +33,8 @@ gzip -k -f Packages
 
 echo "::info::Creating the Release, Release.gpg and InRelease files"
 apt-ftparchive release . >Release
-gpg --default-key "${EMAIL}" --passphrase $PPA_GPG_PASSPHRASE -abs -o - Release >Release.gpg
-gpg --default-key "${EMAIL}" --passphrase $PPA_GPG_PASSPHRASE --clearsign -o - Release >InRelease
+gpg --default-key "${EMAIL}" --passphrase $GPG_PASSPHRASE -abs -o - Release >Release.gpg
+gpg --default-key "${EMAIL}" --passphrase $GPG_PASSPHRASE --clearsign -o - Release >InRelease
 
 echo "::info::Creating the my_list_file.list file"
 echo "deb https://$GITHUB_USERNAME.github.io/monitor-ppa ./" >my_list_file.list
