86e4f6a4665a82b68076e024bcc25980116e9efc
Dima <dima.scherbatyuk@gmail.com>
2022-09-18 16:58:06 +0300

    na: fixing signing Release file

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -34,6 +34,7 @@ echo "::info::Creating the Release, Release.gpg and InRelease files"
 apt-ftparchive release . >Release
 echo "::info:: $(cat $Release)"
 
+export GPG_TTY=$(tty)
 gpg --no-tty --default-key "$EMAIL" --passphrase "$GPG_PASSPHRASE" -abs -o - Release >Release.gpg
 gpg --no-tty --default-key "$EMAIL" --passphrase "$GPG_PASSPHRASE" --clearsign -o - Release >InRelease
 
