59740260743be824ac894b46ba5ff9f94d42b019
Dima <dima.scherbatyuk@gmail.com>
2022-09-20 19:21:30 +0300

    na: download release

--- .github/scripts/gh-dl-release.sh
+++ .github/scripts/gh-dl-release.sh
@@ -30,8 +30,6 @@
 #VERSION="$1" # tag name or the word "latest"
 GITHUB="https://api.github.com"
 
-alias errcho='>&2 echo'
-
 function gh_curl() {
   curl -H "Authorization: token $TOKEN" \
     -H "Accept: application/vnd.github.v3.raw" \
@@ -47,11 +45,11 @@ else
 fi
 
 if [ -z "$asset_id" ]; then
-  errcho "ERROR: version not found $VERSION"
+  echo >&2 "ERROR: version not found $VERSION"
   exit 1
 fi
 if [ "$asset_id" = "null" ]; then
-  errcho "ERROR: file $FILE not found in version $VERSION"
+  echo >&2 "ERROR: file $FILE not found in version $VERSION"
   exit 2
 fi
 
